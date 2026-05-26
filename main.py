00c156af323050f4931405d174b2abc07f9caeea
tejaravutla287 <tejaravutla287@gmail.com>
2026-05-26 09:16:38 +0000

    chore: align architecture and force clean sync trigger

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ class MyHandler(SimpleHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(b"<h1>Welcome to the Python GitOps POC!</h1>")
+        self.wfile.write(b"<h1>Welcome to the LIVE Python GitOps POC Application!</h1>")
 
 if __name__ == "__main__":
     server = HTTPServer(('0.0.0.0', 8080), MyHandler)
-    print("Python server running on port 8080...")
+    print("Python app running on port 8080...")
     server.serve_forever()
